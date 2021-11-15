1966d3b64f141f5de57168d978017d4d75a14717
Marek Caputa <me@mark97.net>
2021-11-15 18:54:42 +0100

    Task 1

--- app/resources/ingredient_resource.rb
+++ app/resources/ingredient_resource.rb
@@ -2,4 +2,7 @@ class IngredientResource < ApplicationResource
   attribute :name, :string
   attribute :unit, :string_enum, allow: Ingredient.units.keys
   attribute :category, :string_enum, allow: Ingredient.categories.keys
+
+  filter :unit, :string_enum, allow: Ingredient.units.keys
+  filter :category, :string_enum, allow: Ingredient.categories.keys
 end
